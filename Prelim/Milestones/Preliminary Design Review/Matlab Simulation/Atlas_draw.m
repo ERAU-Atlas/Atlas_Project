2adcc0772a3f789ee701030b27febc985e28b98d
U-ERAU\adelmani <adelmani@B72-127-SUPR-10.erau.edu>
2023-10-27 12:47:32 -0700

    created PDR folder and started Matlab Simulation Code

---
+++ Prelim/Milestones/Preliminary Design Review/Matlab Simulation/Atlas_draw.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Atlas_draw(inputArg1,inputArg2)
+%ATLAS_DRAW Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
